a5635725c8748d946294e480012dda7ea072ca3c
Prakarsh Verma <prakarsh1248@gmail.com>
2024-07-20 11:46:26 +0530

    Enums

--- src/a.ts
+++ src/a.ts
@@ -78,3 +78,20 @@ function maxValue(arr: number[]): number {
     }
     return maxi;
 }
+
+//enum 
+
+// // const app = express();
+
+// enum ResponseStatus {
+//     Success = 200,
+//     NotFound = 404,
+//     Error = 500
+// }
+
+// app.get("/", (req,res)=>{
+//     if(!req.query.userId){
+//         res.status(ResponseStatus.NotFound).json({});
+//     }
+//     res.status(ResponseStatus.Success).json({});
+// })
