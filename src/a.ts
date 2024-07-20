04f0a467691eaf78407b5cedd3d67c958723ea21
Prakarsh Verma <prakarsh1248@gmail.com>
2024-07-20 10:45:29 +0530

    Interfaces, Types, Difference btw interface and types, union and intersection in Types, Arrays

--- src/a.ts
+++ src/a.ts
@@ -1,19 +1,19 @@
-let x: number = 1;
-console.log(x);
+// let x:number = 1;
+// console.log(x);
 
-function greet(firstName: string, lastName: string, age: number){
-    console.log("Hello" + firstName);
-}
-greet("Harkirat", "singh", 28);
+// function greet(firstName: string, lastName: string, age: number){
+//     console.log("Hello" + firstName);
+// }
+// greet("Harkirat", "singh", 28);
 
-// Create a function that takes another function as input and runs it after 1 second.
+// // Create a function that takes another function as input and runs it after 1 second.
 
-function runAter1s(fn: () => void){
-    setTimeout(fn, 1000);
-}
-runAter1s(function(){
-    console.log("hi There");
-})
+// function runAter1s(fn: () => void){
+//     setTimeout(fn, 1000);
+// }
+// runAter1s(function(){
+//     console.log("hi There");
+// })
 
 //Create a function that return true if user is above legal age
 
@@ -28,20 +28,53 @@ runAter1s(function(){
 // }
 
 //so we use interfaces
-interface User{
-    firstName: string;
-    lastName: string;
-    age : number;
-    email?: string; //you can pass email or cannot pass email
-};
+// interface User{
+//     firstName: string;
+//     lastName: string;
+//     age : number;
+//     email?: string; //you can pass email or cannot pass email
+// };
 
-function isLegalAge(user: User) : boolean{  //user is a object
-    if(user.age > 18) return true;
-    else return false;
+// function isLegalAge(user: User) : boolean{  //user is a object
+//     if(user.age > 18) return true;
+//     else return false;
+// }
+
+// isLegalAge({
+//     firstName: "Harkirat",
+//     lastName: "singh",
+//     age : 20
+// })
+
+
+// // Implement interface
+// interface Person{
+//     name: string;
+//     age: number;
+//     greet(phrases: string) : void;
+// }
+// class Employee implements Person{
+//     name: string;
+//     age: number;
+
+//     constructor(n: string, a: number){
+//         this.name = n;
+//         this.age = a;
+//     }
+
+//     greet(phrases: string){
+//         console.log(`${phrases} ${this.name}`);
+//     }
+// }
+
+//Arrays in TS
+
+function maxValue(arr: number[]): number {
+    let maxi = 0;
+    for(let i=0; i<arr.length; i++){
+        if(arr[i]>maxi){
+            maxi = arr[i];
+        }
+    }
+    return maxi;
 }
-
-isLegalAge({
-    firstName: "Harkirat",
-    lastName: "singh",
-    age : 20
-})
